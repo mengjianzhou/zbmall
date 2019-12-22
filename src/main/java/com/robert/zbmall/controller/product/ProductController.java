9b923faf5f0dde8adcfcab944ef66a83b6509126
mengjianzhou <362601125@qq.com>
2019-12-22 19:06:19 +0800

    引入前端layui 框架

---
+++ src/main/java/com/robert/zbmall/controller/product/ProductController.java
@@ -0,0 +1,15 @@
+package com.robert.zbmall.controller.product;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 产品
+ *
+ * @author zhaob
+ * @create 2019/12/22
+ */
+@RestController
+public class ProductController {
+
+    
+}
---
+++ src/main/java/com/robert/zbmall/service/product/impl/ProductServiceImpl.java
@@ -0,0 +1,17 @@
+package com.robert.zbmall.service.product.impl;
+
+import com.robert.zbmall.service.product.ProductService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈${DESCRIPTION}〉
+ *
+ * @author zhaob
+ * @create 2019/12/22
+ */
+@Service
+public class ProductServiceImpl implements ProductService {
+
+
+}
